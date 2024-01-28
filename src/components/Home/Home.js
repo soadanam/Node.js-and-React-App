c5b6217e2afdc3326d39be1f26d08caede7a7111
soadam <soad.anam7@gmail.com>
2024-01-28 14:59:40 +0600

    added some indicating text and style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,11 +10,15 @@ const Home = () => {
     const { user } = useAuth();
 
     return (
-        <div>
-            <h1 className='homeHeader'>This is homepage!</h1>
+        <div className='home-body'>
+            <h2 className='homeHeader'>This is homepage!</h2>
 
             {user.email ?
-                <img src="https://www.shutterstock.com/image-vector/protected-stamp-red-round-grunge-260nw-515971792.jpg" alt="random img" /> :
+                <div>
+                    <h4 className='protected-text'>Only for logged in user:</h4>
+                    <img src="https://www.shutterstock.com/image-vector/protected-stamp-red-round-grunge-260nw-515971792.jpg" alt="random img" />
+                </div>
+                :
                 <a href="/login" className='toLoginNavigateLink'> Please Login to get the full access! </a>
             }
 
