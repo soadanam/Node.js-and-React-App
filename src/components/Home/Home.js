1b4638e22e237124723bf94062b966725ef4ecad
soadam <soad.anam7@gmail.com>
2024-01-29 00:40:50 +0600

    added alt to img

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
             {user.email ?
                 <div>
                     <h4 className='protected-text'>Only for logged in user:</h4>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOKCOETDGlCCei9vpbYfpATOaQwsyUsi23tE1yIXKQ60YX68KvmcayGrEY6ZV7G3xYnFk&usqp=CAU" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOKCOETDGlCCei9vpbYfpATOaQwsyUsi23tE1yIXKQ60YX68KvmcayGrEY6ZV7G3xYnFk&usqp=CAU" alt="random img" />
                 </div>
                 :
                 <a href="/login" className='toLoginNavigateLink'> Please Login to get the full access! </a>
